6bfde25ee4557125a64c1b5691b3006b75bf432c
Jordan365-sudo <writersmart365@gmail.com>
2026-01-30 12:41:34 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./**/*.{js,ts,jsx,tsx,html}",
   ],
-  prefix: "",
   theme: {
     container: {
       center: true,
